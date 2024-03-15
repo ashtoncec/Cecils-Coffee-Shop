be38577c7c463eaea6ee0c812b03a72c4a5a042a
ashtoncec <123290547+ashtoncec@users.noreply.github.com>
2024-03-15 16:31:58 -0500

    Initial Commit

---
+++ CecilsCoffeeShop/CecilsCoffeeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CecilsCoffeeShopApp.swift
+//  CecilsCoffeeShop
+//
+//  Created by Ashton Cecil on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CecilsCoffeeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
